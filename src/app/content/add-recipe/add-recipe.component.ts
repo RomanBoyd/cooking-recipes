981e43ddb902ecc5c98401aff4d98d8f6a4f2b43
cityman177 <cityman177@gmail.com>
2021-08-19 18:55:03 +0300

    add recipe form

--- src/app/content/add-recipe/add-recipe.component.ts
+++ src/app/content/add-recipe/add-recipe.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormArray, FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-add-recipe',
@@ -7,9 +8,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddRecipeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
+
+  form: FormGroup;
+  ingredients: FormArray;
+  steps: FormArray;
 
   ngOnInit(): void {
+    this.form = this.fb.group({
+      name: this.fb.control('', [Validators.required]),
+      ingredients: this.fb.array([this.createIngredient()]),
+      steps: this.fb.array([this.createStep()]),
+      image: this.fb.control('', [Validators.required])
+    });
+  }
+
+  createIngredient(): FormGroup {
+    return this.fb.group({
+      ingredient: this.fb.control('', [Validators.required]),
+      measure: this.fb.control('', [Validators.required])
+    });
+  }
+
+  addIngredient(): void {
+    this.ingredients = this.form.get('ingredients') as FormArray;
+    this.ingredients.push(this.createIngredient());
+  }
+
+  deleteIngredient(): void {
+    if (this.ingredients.length > 1) {
+      this.ingredients = this.form.get('ingredients') as FormArray;
+      this.ingredients.removeAt(this.ingredients.length - 1);
+    }
+  }
+
+  createStep(): FormGroup {
+    return this.fb.group({
+      step: this.fb.control('', [Validators.required])
+    });
+  }
+
+  addStep(): void {
+    this.steps = this.form.get('steps') as FormArray;
+    this.steps.push(this.createStep());
+  }
+
+  deleteStep(): void {
+    if (this.steps.length > 1) {
+      this.steps = this.form.get('steps') as FormArray;
+      this.steps.removeAt(this.steps.length - 1);
+    }
+  }
+
+  addRecipe(): void {
+    this.form.reset();
   }
 
 }
